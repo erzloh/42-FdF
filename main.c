0db8d3af2615ba91ee8598220bc639ea24a2e847
Eric Holzer <eric.holzer.lsn@gmail.com>
2022-12-20 14:27:23 +0100

    Started working on the parsing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eholzer <eholzer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 08:45:15 by eholzer           #+#    #+#             */
-/*   Updated: 2022/12/20 10:40:08 by eholzer          ###   ########.fr       */
+/*   Updated: 2022/12/20 14:27:23 by eholzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,23 @@ int	main(void)
 	t_point	p1;
 	t_point	p2;
 	char	**map;
+	char	*test;
+	int		*tab_x;
+	int		tab_size;
+	int		i;
+
+	// ---------------------- test -----------------------------------
+	test = "0  0 10 10  0  0 10 10  0  0  0 10 10 10 10 10  0  0  0";
+	tab_size = get_tab_size(test);
+	printf("size: %d\n", tab_size);
+	tab_x = get_tab_x(test);
+	i = 0;
+	while (i < tab_size)
+	{
+		printf("tab_x[%d] = %d\n", i, tab_x[i]);
+		i++;
+	}
+	// ---------------------- test -----------------------------------
 
 	map = ft_split("0000 0100 0000", ' ');
 	p1.x = 250;
@@ -37,6 +54,7 @@ int	main(void)
 	//  draw_line(mlxd, p1, p2);
 	draw_grid(mlxd, map);
 	draw_iso_grid(mlxd, map);
+	
 	//mlx_key_hook(mlxd.win_ptr, deal_key, (void *)0);
 	mlx_loop(mlxd.mlx_ptr);
 }
